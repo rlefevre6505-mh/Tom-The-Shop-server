80980000001223db701cf2519568ae61fcfa8400
Richard Lefevre <rlefevre.6505@gmail.com>
2026-04-19 12:31:29 +0100

    commit to test

--- server/server.ts
+++ server/server.ts
@@ -15,23 +15,25 @@ app.get("/", (request, response) =>
 //TODO: test for db connection and date functionality
 //
 app.post("/set-date", (request, response) => {
-  const reviewForm = request.body.formValues;
-  const query = db.query(`INSERT INTO date_test () VALUES ($1)`, [
-    reviewForm.date,
-  ]);
-  response.json({ status: "success", values: reviewForm });
+  const form = request.body.formValues;
+  const query = db.query(
+    `INSERT INTO date_test (event_title, start_date, end_date) VALUES ($1, $2, $3)`,
+    [form.event_title, form.start_date, form.end_date],
+  );
+  response.json({ status: "success", values: form });
 });
 
 // GET REQUESTS
 //
 app.get("/stored-events", async function (request, response) {
   const query = await db.query(
-    `SELECT name, location, date_in, date_out, comments FROM hotelreviews;`,
+    `SELECT event_title, start_date, end_date FROM date_test;`,
   );
   console.log(query.rows);
   const data = response.json(query.rows);
 });
 
+//START SERVER
 app.listen(PORT, () => {
   console.info(`Server is running in port ${PORT}`);
 });

71c2290baeea2c08f40f3051507092e53c9110e1
Richard Lefevre <rlefevre.6505@gmail.com>
2026-04-20 11:13:26 +0100

    added further parameters to date parsing

--- server/dbconnection.ts
+++ server/dbconnection.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 pg.types.setTypeParser(1082, (value) => value);
+pg.types.setTypeParser(1114, (value) => value);
+pg.types.setTypeParser(1184, (value) => value);
 
 export const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,

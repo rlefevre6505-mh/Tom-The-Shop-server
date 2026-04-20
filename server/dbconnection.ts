8885b51f4ca8e0f468a2bf1976db96ee9839a84a
Richard Lefevre <rlefevre.6505@gmail.com>
2026-04-20 11:11:57 +0100

    addded type parsing to dates

--- server/dbconnection.ts
+++ server/dbconnection.ts
@@ -2,6 +2,7 @@ import pg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+pg.types.setTypeParser(1082, (value) => value);
 
 export const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,

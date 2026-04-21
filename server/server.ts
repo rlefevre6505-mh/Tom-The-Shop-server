a37038251d7859ad69ce50e9777695e2d77b8955
Richard Lefevre <rlefevre.6505@gmail.com>
2026-04-21 15:26:45 +0100

    added get route  for selected event

--- server/server.ts
+++ server/server.ts
@@ -6,35 +6,49 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = 8080;
-app.get("/", (request, response) =>
-  response.json({ message: "Welcome to the Tom The Shop Logistics server" }),
+app.get("/", (req, res) =>
+  res.json({ message: "Welcome to the Tom The Shop Logistics server" }),
 );
 
 // POST REQUESTS
 //
-app.post("/set-date", (request, response) => {
+app.post("/set-date", (req, res) => {
   try {
-    const form = request.body;
-
-    // TODO: add "await"
+    const form = req.body;
     const query = db.query(
       `INSERT INTO date_test (title, "start", "end") VALUES ($1, $2, $3)`,
       [form.title, form.start, form.end],
     );
-    response.json({ status: "success", values: form });
+    res.json({ status: "success", values: form });
   } catch (error) {
     console.error(`Error: ${error}`);
   }
 });
 
 // GET REQUESTS
-//
-app.get("/stored-events", async function (request, response) {
-  const query = await db.query(
-    `SELECT id, title, "start", "end" FROM date_test;`,
-  );
-  console.log(query.rows);
-  const data = response.json(query.rows);
+// get all events for CalendarView
+app.get("/stored-events", async function (req, res) {
+  try {
+    const query = await db.query(
+      `SELECT id, title, "start", "end" FROM date_test;`,
+    );
+    const data = res.json(query.rows);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+});
+// get event for SelectedEventView
+app.get("/selected-event", async function (req, res) {
+  try {
+    const { id } = req.body;
+    const query = await db.query(`SELECT * FROM date_test WHERE id = $1;`, [
+      id,
+    ]);
+    const data = res.json(query.rows[0]);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 //START SERVER
